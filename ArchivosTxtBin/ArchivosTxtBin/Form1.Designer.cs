bab642875e797956c1e58990e247ea1b48da12a0
Osgen <ovirgen0@ucol.mx>
2018-04-14 17:19:56 -0500

    Add files via upload

---
+++ ArchivosTxtBin/ArchivosTxtBin/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace ArchivosTxtBin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bttnAbrir = new System.Windows.Forms.Button();
+            this.txtDatos = new System.Windows.Forms.TextBox();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.bttnXml = new System.Windows.Forms.Button();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtGenero = new System.Windows.Forms.TextBox();
+            this.txtPlataforma = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bttnAbrir
+            // 
+            this.bttnAbrir.Location = new System.Drawing.Point(220, 42);
+            this.bttnAbrir.Name = "bttnAbrir";
+            this.bttnAbrir.Size = new System.Drawing.Size(75, 23);
+            this.bttnAbrir.TabIndex = 0;
+            this.bttnAbrir.Text = "Abrir Archivo";
+            this.bttnAbrir.UseVisualStyleBackColor = true;
+            this.bttnAbrir.Click += new System.EventHandler(this.bttnAbrir_Click);
+            // 
+            // txtDatos
+            // 
+            this.txtDatos.Location = new System.Drawing.Point(46, 69);
+            this.txtDatos.Multiline = true;
+            this.txtDatos.Name = "txtDatos";
+            this.txtDatos.Size = new System.Drawing.Size(151, 99);
+            this.txtDatos.TabIndex = 1;
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(73, 47);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(109, 20);
+            this.txtTitulo.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtPrecio);
+            this.groupBox1.Controls.Add(this.txtPlataforma);
+            this.groupBox1.Controls.Add(this.txtGenero);
+            this.groupBox1.Controls.Add(this.txtDescripcion);
+            this.groupBox1.Controls.Add(this.bttnXml);
+            this.groupBox1.Controls.Add(this.txtTitulo);
+            this.groupBox1.Location = new System.Drawing.Point(12, 224);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(279, 210);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Videojuego";
+            // 
+            // bttnXml
+            // 
+            this.bttnXml.Location = new System.Drawing.Point(193, 169);
+            this.bttnXml.Name = "bttnXml";
+            this.bttnXml.Size = new System.Drawing.Size(75, 23);
+            this.bttnXml.TabIndex = 4;
+            this.bttnXml.Text = "GenerarXML";
+            this.bttnXml.UseVisualStyleBackColor = true;
+            this.bttnXml.Click += new System.EventHandler(this.bttnXml_Click);
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(73, 91);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(109, 20);
+            this.txtDescripcion.TabIndex = 5;
+            // 
+            // txtGenero
+            // 
+            this.txtGenero.Location = new System.Drawing.Point(73, 69);
+            this.txtGenero.Name = "txtGenero";
+            this.txtGenero.Size = new System.Drawing.Size(109, 20);
+            this.txtGenero.TabIndex = 6;
+            // 
+            // txtPlataforma
+            // 
+            this.txtPlataforma.Location = new System.Drawing.Point(73, 113);
+            this.txtPlataforma.Name = "txtPlataforma";
+            this.txtPlataforma.Size = new System.Drawing.Size(109, 20);
+            this.txtPlataforma.TabIndex = 7;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(73, 135);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(109, 20);
+            this.txtPrecio.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Titulo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Genero:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Descripcion:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Plataforma:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Precio:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(43, 42);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Datos de la imagen:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 468);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtDatos);
+            this.Controls.Add(this.bttnAbrir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bttnAbrir;
+        private System.Windows.Forms.TextBox txtDatos;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtPlataforma;
+        private System.Windows.Forms.TextBox txtGenero;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Button bttnXml;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ ArchivosTxtBin/ArchivosTxtBin/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ArchivosTxtBin
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+        SaveFileDialog sfd = new SaveFileDialog();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public bool NoValido(Int16 bm)
+        {
+            if(bm == 19778)
+            {
+                return false ;
+            }
+            return true;
+        }
+
+        public string Datos(Int32 tam, Int32 an, Int32 alt, Int16 np)
+        {
+            string s = "Tamaño: " + tam +" bytes"+ Environment.NewLine + "Ancho: " + an +
+                " Pixeles"+ Environment.NewLine + "Alto: " + alt+ " Pixeles"+Environment.NewLine+"No.Bit/Px: "+np;
+            return s;
+        }
+
+        public string LeerArchivo()
+        {
+            ofd.ShowDialog();
+            FileStream archivo = new FileStream(ofd.FileName, FileMode.Open);
+            BinaryReader br = new BinaryReader(archivo);
+           
+            Int16 bm = br.ReadInt16();
+          
+            if (NoValido(bm))
+            {
+                br.Close();
+                archivo.Close();
+                return "No es un archivo BMP";
+            }
+            Int32 tamaño = br.ReadInt32();
+            for(int i =0; i<12; i++)
+            {
+                br.Read();
+            }
+            Int32 ancho = br.ReadInt32();
+            Int32 alto = br.ReadInt32();
+            for (int i = 0; i < 2; i++)
+            {
+                br.Read();
+            }
+            Int16 nPixel = br.ReadInt16();
+            br.Close();
+            archivo.Close();
+            return Datos(tamaño, ancho, alto,nPixel);
+        }
+
+        private void bttnAbrir_Click(object sender, EventArgs e)
+        {
+            txtDatos.Text= LeerArchivo();
+        }
+
+        public void GenerarXml(string tit, string genero, string des, string plat, string precio)
+        {
+            sfd.ShowDialog();
+            FileStream archivo = new FileStream(sfd.FileName, FileMode.Create);
+            StreamWriter sw = new StreamWriter(archivo);
+            sw.WriteLine("<?xml version=\"1.0\" encoding =\"UTF - 8\" ?>");
+            sw.WriteLine("<CATALOGO>");
+            sw.WriteLine("  <JUEGO1>");
+            sw.WriteLine("      <TITULO>" + tit + "</TITULO>");
+            sw.WriteLine("      <GENERO>"+genero+"</GENERO>");
+            sw.WriteLine("      <DESCRIPCION>"+des+"</DESCRIPCION>");
+            sw.WriteLine("      <PLATAFORMA>" + plat + "</PLATAFORMA>");
+            sw.WriteLine("      <PRECIO>"+precio+"</PRECIO>");
+            sw.WriteLine("  </JUEGO1>");
+            sw.WriteLine("</CATALOGO>");
+            sw.Close();
+            archivo.Close();
+        }
+
+        private void bttnXml_Click(object sender, EventArgs e)
+        {
+            GenerarXml(txtTitulo.Text, txtGenero.Text, txtDescripcion.Text, txtPlataforma.Text, txtPrecio.Text);
+
+        }
+    }
+}
